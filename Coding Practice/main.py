a4387ee82e7b08be58be625e7ad5024a9d9f0e21
Rishi <83863516+RishiRatanPandey@users.noreply.github.com>
2022-02-07 18:37:29 +0530

    Add files via upload

---
+++ Coding Practice/main.py
@@ -0,0 +1,30 @@
+# ex1:-from:https://adriann.github.io/programming_problems.html from section list,string
+'''
+problem statement:-Write a function that takes a number and returns 
+a list of its digits.So for 2342 it should return [2,3,4,2]
+'''
+# def ex1():
+#     list1=[]
+#     a=input('Enter Number:')
+#     len_of_users_input=len(a)
+#     for x in range(len_of_users_input):
+#         list1.append(int(a[x]))
+#     print(list1)
+# ex1()
+# -------------------------------------------------------------------
+# ex2:-same website
+'''Write a function that combines two lists by alternatingly taking elements, 
+e.g. [a,b,c], [1,2,3] → [a,1,b,2,c,3].'''
+# def ex2():
+#     list1=['a','b','c']
+#     list2=[1,2,3]
+#     list3=[]
+#     for x in range(len(list1)):
+#         if len(list1)==len(list2):
+#             list3.append(list1[x])
+#             try:
+#                 list3.append(list2[x])
+#             except:
+#                 pass
+#     print(list3)
+# ex2() 
